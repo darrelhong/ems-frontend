02c227424b487d02367f72b89ce921eeb6fe5c67
darrelhong <darrel.hong@gmail.com>
2021-01-17 13:21:19 +0800

    add react-query + graphql-request and some layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-import {Fonts} from '../styles/Fonts'
+import { Fonts } from "../styles/Fonts";
 
-import theme from '../styles/theme'
+import theme from "../styles/theme";
 import "../styles/globals.css";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Fonts />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Fonts />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,109 @@
 import Head from "next/head";
-import { Box, Text, Link, VStack, Code, Grid, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  Heading,
+  Flex,
+  Spacer,
+  Container,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useQuery } from "react-query";
+import { request, gql } from "graphql-request";
 
 import { ColorModeSwitcher } from "../components/ColorModeSwitcher";
 
-export default function Home() {
+export default function Home({
+  launchesPast,
+}: {
+  launchesPast: Array<Object>;
+}) {
+  console.log(launchesPast);
+
+  const boxBg = useColorModeValue("gray.50", "gray.700");
+
   return (
     <>
       <Head>
         <title>Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* nav */}
+      <Flex w="100%" p={2} borderBottom="1px" borderColor="gray.200">
+        <Spacer />
+        <ColorModeSwitcher />
+      </Flex>
 
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Heading></Heading>
-            <Text>
-              Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+      {/* responsive container */}
+      <Container
+        w="100%"
+        maxW={["100%", "100%", "60em", "60em", "72em"]}
+        minH="100vh"
+        p={3}
+      >
+        <Heading textAlign="center" mb={3}>
+          SpaceX Land
+        </Heading>
+        <Grid rowGap={2}>
+          {launchesPast.map((launch, index) => (
+            <Box bg={boxBg} borderRadius="md" key={index} p={2}>
+              <Text color="pink.400">{launch.mission_name}</Text>
+            </Box>
+          ))}
         </Grid>
-      </Box>
+      </Container>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const { launchesPast } = await request(
+    "https://api.spacex.land/graphql/",
+    gql`
+      {
+        launchesPast(limit: 10) {
+          mission_name
+          launch_date_local
+          launch_site {
+            site_name_long
+          }
+          links {
+            article_link
+            video_link
+          }
+          rocket {
+            rocket_name
+            first_stage {
+              cores {
+                flight
+                core {
+                  reuse_count
+                  status
+                }
+              }
+            }
+            second_stage {
+              payloads {
+                payload_type
+                payload_mass_kg
+                payload_mass_lbs
+              }
+            }
+          }
+          ships {
+            name
+            home_port
+            image
+          }
+        }
+      }
+    `
+  );
+  return {
+    props: { launchesPast },
+  };
 }
