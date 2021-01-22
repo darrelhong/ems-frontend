c6c09597b0443840fc8e547edd192b935c476acb
darrelhong <darrel.hong@gmail.com>
2021-01-22 12:22:13 +0800

    simple example with JPA and MySQL

---
+++ backend/src/main/java/com/is4103/backend/example/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.is4103.backend.example;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
+
+}
