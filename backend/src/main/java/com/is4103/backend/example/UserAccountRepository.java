0d68936c6be6f31fd4acd94b9f5eb97a6e0cf774
darrelhong <darrel.hong@gmail.com>
2021-01-24 21:13:02 +0800

    remove backend code

--- backend/src/main/java/com/is4103/backend/example/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.is4103.backend.example;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
-
-}
