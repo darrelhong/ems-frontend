7c8dc15da472bc1a9103d4505c953a75e0512316
darrelhong <darrel.hong@gmail.com>
2021-02-03 08:25:27 +0800

    show organiser details

--- pages/organiser/home.tsx
+++ pages/organiser/home.tsx
@@ -1,20 +1,32 @@
 import Head from 'next/head';
-import { Heading } from '@chakra-ui/react';
+import { Heading, Skeleton, Text } from '@chakra-ui/react';
 
 import NavBar from '../../components/NavBar/NavBar';
 import PageContainer from '../../components/PageContainer';
 import withProtectRoute from '../../components/ProtectRouteWrapper';
+import useUser from '../../lib/query/useUser';
 
 function OrganiserHome(): JSX.Element {
+  const { data: user, isLoading, isSuccess } = useUser(
+    localStorage.getItem('userId')
+  );
   return (
     <>
       <Head>
         <title>Organiser Dasboard</title>
       </Head>
+
       <NavBar />
 
       <PageContainer>
         <Heading>Organiser Home</Heading>
+        {isLoading && <Skeleton height="40px" />}
+        {isSuccess && (
+          <>
+            <Text>Name: {user?.name}</Text>
+            <Text>User ID: {user?.id}</Text>
+          </>
+        )}
       </PageContainer>
     </>
   );
