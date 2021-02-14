1c0c6699513013a4ab6696362c5057a295b85a9f
darrelhong <darrel.hong@gmail.com>
2021-02-14 21:14:00 +0800

    setup organiser settings

--- pages/organiser/home.js
+++ pages/organiser/home.js
@@ -1,28 +1,60 @@
-import { Heading, Skeleton, Text } from '@chakra-ui/react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { Container } from 'react-bootstrap';
 
 import useUser from '../../lib/query/useUser';
 
-import { OrganiserPageWrapper } from '../../components/wrapper/PageWrapper';
 import withProtectRoute from '../../components/ProtectRouteWrapper';
-import ChakraWrapper from '../../components/ChakraWrapper';
+import { BreadcrumbOne } from '../../components/Breadcrumb';
+import { FooterOne } from '../../components/Footer';
+import OrganiserHeader from '../../components/Header/OrganiserHeader';
 
 function OrganiserHome() {
   const { data: user, isLoading, isSuccess } = useUser(
     localStorage.getItem('userId')
   );
   return (
-    <ChakraWrapper>
-      <OrganiserPageWrapper title="Organiser Dashboard">
-        <Heading>Organiser Home</Heading>
-        {isLoading && <Skeleton height="40px" />}
+    <>
+      <Head>
+        <title>Organiser Home</title>
+      </Head>
+
+      <OrganiserHeader />
+
+      <BreadcrumbOne pageTitle="Organiser Home">
+        <ol className="breadcrumb justify-content-md-end">
+          <li className="breadcrumb-item">
+            <Link href="/organiser/home">
+              <a>Organiser Home</a>
+            </Link>
+          </li>
+        </ol>
+      </BreadcrumbOne>
+
+      <Container>
+        {isLoading && <div className="spinner-grow" role="status" />}
         {isSuccess && (
           <>
-            <Text>Name: {user?.name}</Text>
-            <Text>User ID: {user?.id}</Text>
+            <p>{user?.name}</p>
+            <p>User ID: {user?.id}</p>
           </>
         )}
-      </OrganiserPageWrapper>
-    </ChakraWrapper>
+      </Container>
+
+      <FooterOne />
+    </>
+    // <ChakraWrapper>
+    //   <OrganiserPageWrapper title="Organiser Dashboard">
+    //     <Heading>Organiser Home</Heading>
+    //     {isLoading && <Skeleton height="40px" />}
+    //     {isSuccess && (
+    //       <>
+    //         <Text>Name: {user?.name}</Text>
+    //         <Text>User ID: {user?.id}</Text>
+    //       </>
+    //     )}
+    //   </OrganiserPageWrapper>
+    // </ChakraWrapper>
   );
 }
 
