e55ab05b3ccba23bf40a229e2c5ba9be32431e53
keithhjc <keithhjc@gmail.com>
2021-02-22 09:14:43 +0800

    Refactor api to use eventapi

--- pages/organiser/events/index.js
+++ pages/organiser/events/index.js
@@ -2,21 +2,33 @@ import React, { useState, useEffect } from "react";
 import Link from 'next/link';
 import { connect } from "react-redux";
 import { BreadcrumbOne } from '../../../components/Breadcrumb';
+import { Container, Row, Col } from "react-bootstrap";
 import OrganiserWrapper from '../../../components/wrapper/OrganiserWrapper';
-import api from "../../../lib/ApiClient";
+import { getAllEvents } from "../../../lib/query/eventApi";
+import { LayoutOne } from "../../../layouts";
+import { Sidebar, ShopHeader, ShopProducts } from "../../../components/Shop";
+
 
 
 function myEvents() {
     const [events, setEvents] = useState([]);
+    //test
+    const [layout, setLayout] = useState("list");
+    const [offset, setOffset] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentData, setCurrentData] = useState([]);
+
+    const pageLimit = 12;
+
 
     useEffect(() => {
         const getEvents = async () => {
-            const { data } = await api.get("/api/event/all");
-
+            const data = await getAllEvents();
             setEvents(data);
         }
         getEvents();
-    }, []);
+        // setCurrentData(events.slice(offset, offset + pageLimit));
+    });
 
     return (
         <div>
@@ -33,6 +45,9 @@ function myEvents() {
                 </BreadcrumbOne>
             </OrganiserWrapper>
 
+            <Container>
+
+            </Container>
 
 
         </div>
