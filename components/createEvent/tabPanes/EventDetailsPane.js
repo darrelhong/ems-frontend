0be571f2374797c426c6da7d8ca87af1d1cbfe36
marcusoyh <ongmarcus204@gmail.com>
2021-02-23 15:51:22 +0800

    added useform to createevent

--- components/createEvent/tabPanes/EventDetailsPane.js
+++ components/createEvent/tabPanes/EventDetailsPane.js
@@ -1,7 +1,17 @@
 import { Row, Col } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+// import Cookies from 'js-cookie';
+import { useForm } from 'react-hook-form';
+
+// const token = Cookies.get('token');
 
 const EventDetailsPane = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <Card className="my-account-content__content">
       <Card.Header>
@@ -9,7 +19,7 @@ const EventDetailsPane = () => {
       </Card.Header>
       <Card.Body>
         <div className="account-details-form">
-          <form method="post" name="enq">
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Row>
               <Col className="form-group" md={12}>
                 <label>
@@ -20,6 +30,19 @@ const EventDetailsPane = () => {
                   className="form-control"
                   name="name"
                   type="text"
+                  ref={register()}
+                />
+              </Col>
+              <Col className="form-group" md={12}>
+                <label>
+                  Address <span className="required">*</span>
+                </label>
+                <input
+                  required
+                  className="form-control"
+                  name="address"
+                  type="text"
+                  ref={register()}
                 />
               </Col>
               <Col className="form-group" md={12}>
@@ -29,8 +52,37 @@ const EventDetailsPane = () => {
                 <textarea
                   required
                   className="form-control"
-                  name="description"
+                  name="descriptions"
                   maxLength="5" //can consider playing with this if needed
+                  ref={register()}
+                />
+              </Col>
+              <Col className="form-group" md={12}>
+                <label>Event Website (if any)</label>
+                <input
+                  className="form-control"
+                  name="website"
+                  type="text"
+                  ref={register()}
+                />
+              </Col>
+              <Col className="form-group" md={12}>
+                <label>Ticket Price</label>
+                <input
+                  className="form-control"
+                  name="ticketPrice"
+                  type="number"
+                  step="0.1"
+                  ref={register()}
+                />
+              </Col>
+              <Col className="form-group" md={12}>
+                <label>Ticket Capacity</label>
+                <input
+                  className="form-control"
+                  name="ticketCapacity"
+                  type="number"
+                  ref={register()}
                 />
               </Col>
               <Col md={12}>
