81aa16165833ec8fd64c8406e3ef16e52356f374
darrelhong <darrel.hong@gmail.com>
2021-02-24 18:17:14 +0800

    update change forgotten password page

--- pages/register/reset-password/verify.js
+++ pages/register/reset-password/verify.js
@@ -2,28 +2,16 @@ import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import Link from 'next/link';
-import {
-  Alert,
-  AlertIcon,
-  Button,
-  Container,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Grid,
-  GridItem,
-  Heading,
-} from '@chakra-ui/react';
+import cx from 'classnames';
+
 import { useMutation } from 'react-query';
 import { useForm } from 'react-hook-form';
 
 import api from '../../../lib/ApiClient';
 
-import NavBar from '../../../components/NavBar/NavBar';
-import PageContainer from '../../../components/PageContainer';
-import Card from '../../../components/Card';
-import PasswordInput from '../../../components/settings/PasswordInput';
-import ChakraWrapper from '../../../components/ChakraWrapper';
+import { LayoutOne } from '../../../layouts';
+import { Alert, Container } from 'react-bootstrap';
+import ButtonWithLoading from '../../../components/custom/ButtonWithLoading';
 
 export const getServerSideProps = async ({ query }) => {
   return {
@@ -47,94 +35,95 @@ export default function ResetPassword({ token }) {
   };
 
   return (
-    <ChakraWrapper>
+    <>
       <Head>
         <title>Password Reset</title>
       </Head>
 
-      <NavBar />
+      <LayoutOne>
+        <Container className="my-4">
+          <div className="d-flex justify-content-center">
+            <div>
+              <h2>Change password</h2>
+              {isError && (
+                <Alert variant="danger">
+                  An error occurred. Please try again.
+                </Alert>
+              )}
 
-      <PageContainer centerContent>
-        <Heading mt={4} mb={3} textAlign="center" size="lg">
-          Change password
-        </Heading>
+              {isSuccess ? (
+                <Alert varaint="success">
+                  Your password has been updated successfully. Please proceed to
+                  login.
+                  <Link href="/">
+                    <button className="btn btn-fill-out btn-sm">
+                      Back to home
+                    </button>
+                  </Link>
+                </Alert>
+              ) : (
+                <div>
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-group">
+                      <label htmlFor="password">New password</label>
+                      <input
+                        name="password"
+                        id="password"
+                        type="password"
+                        ref={register({
+                          required: 'Password is required',
+                          minLength: {
+                            value: 8,
+                            message: 'Password must have at least 8 characters',
+                          },
+                        })}
+                        placeholder="Enter new password"
+                        className={cx('form-control', {
+                          'is-invalid': errors?.password,
+                        })}
+                      />
+                      <div className="invalid-feedback">
+                        {errors?.password?.message}
+                      </div>
+                    </div>
 
-        <Container maxW="xs">
-          {isError && (
-            <Alert status="error" mb={3}>
-              <AlertIcon />
-              An error occurred. Please try again.
-            </Alert>
-          )}
+                    <div className="form-group">
+                      <label htmlFor="password_confirm">Confirm password</label>
+                      <input
+                        name="password_confirm"
+                        id="password_confirm"
+                        type="password"
+                        ref={register({
+                          validate: (value) =>
+                            value === password.current ||
+                            'Passwords do not match',
+                        })}
+                        placeholder="Enter new password"
+                        className={cx('form-control', {
+                          'is-invalid': errors?.password_confirm,
+                        })}
+                      />
+                      <div className="invalid-feedback">
+                        {errors.password_confirm &&
+                          errors.password_confirm.message}
+                      </div>
+                    </div>
 
-          {isSuccess ? (
-            <Alert status="success" mb={3}>
-              <Grid rowGap={2}>
-                <GridItem justifySelf="center">
-                  <AlertIcon />
-                </GridItem>
-                Your password has been updated successfully. Please proceed to
-                login.
-                <Link href="/" passHref>
-                  <Button>Back to home</Button>
-                </Link>
-              </Grid>
-            </Alert>
-          ) : (
-            <Card>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <Grid rowGap={3}>
-                  <FormControl isInvalid={errors.password}>
-                    <FormLabel htmlFor="password">New password</FormLabel>
-                    <PasswordInput
-                      name="password"
-                      id="password"
-                      ref={register({
-                        required: 'Password is required',
-                        minLength: {
-                          value: 8,
-                          message: 'Password must have at least 8 characters',
-                        },
-                      })}
-                      placeholder="Enter new password"
-                    />
-                    <FormErrorMessage>
-                      {errors.password && errors.password.message}
-                    </FormErrorMessage>
-                  </FormControl>
-
-                  <FormControl isInvalid={errors.password_confirm}>
-                    <FormLabel htmlFor="password_confirm">
-                      Confirm password
-                    </FormLabel>
-                    <PasswordInput
-                      name="password_confirm"
-                      id="password_confirm"
-                      ref={register({
-                        validate: (value) =>
-                          value === password.current ||
-                          'Passwords do not match',
-                      })}
-                      placeholder="Enter new password"
-                    />
-                    <FormErrorMessage>
-                      {errors.password_confirm &&
-                        errors.password_confirm.message}
-                    </FormErrorMessage>
-                  </FormControl>
-
-                  <GridItem>
-                    <Button mt={2} type="submit" isLoading={isLoading}>
+                    <ButtonWithLoading
+                      className="btn btn-fill-out btn-sm"
+                      type="submit"
+                      isLoading={isLoading}
+                    >
                       Submit
-                    </Button>
-                  </GridItem>
-                </Grid>
-              </form>
-            </Card>
-          )}
+                    </ButtonWithLoading>
+                  </form>
+                </div>
+              )}
+            </div>
+          </div>
         </Container>
-      </PageContainer>
-    </ChakraWrapper>
+      </LayoutOne>
+    </>
   );
 }
 
