bf6fc61ba916bdf7f49aae0ce8e54941c542d9e1
darrelhong <darrel.hong@gmail.com>
2021-02-28 00:13:59 +0800

    add sort by events

--- pages/partner/events/index.js
+++ pages/partner/events/index.js
@@ -1,3 +1,4 @@
+import { Fragment, useState } from 'react';
 import Link from 'next/link';
 import { Alert, Col, Container, Row, Spinner } from 'react-bootstrap';
 import { useInfiniteQuery } from 'react-query';
@@ -7,24 +8,43 @@ import PartnerWrapper from '../../../components/wrapper/PartnerWrapper';
 import api from '../../../lib/ApiClient';
 import EventCard from '../../../components/events/partner/EventCard';
 import ButtonWithLoading from '../../../components/custom/ButtonWithLoading';
-import { Fragment } from 'react';
 
-const getEvents = async (page = 0) => {
-  const { data } = await api.get('/api/event/get-events?page=' + page);
+const getEvents = async (page = 0, sort, sortDir) => {
+  let url = `/api/event/search?page=${page}`;
+  if (sort && sortDir) url += `&sort=${sort}&sortDir=${sortDir}`;
+  const { data } = await api.get(url);
   return data;
 };
 
 function PartnerHome() {
+  const [sortBy, setSortBy] = useState();
   const {
     status,
     data,
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery('events', ({ pageParam = 0 }) => getEvents(pageParam), {
-    getNextPageParam: (lastPage) =>
-      lastPage.last ? false : lastPage.number + 1,
-  });
+  } = useInfiniteQuery(
+    ['events', sortBy?.sort, sortBy?.sortDir],
+    ({ pageParam = 0 }) => getEvents(pageParam, sortBy?.sort, sortBy?.sortDir),
+    {
+      getNextPageParam: (lastPage) =>
+        lastPage.last ? false : lastPage.number + 1,
+    }
+  );
+
+  const handleChange = (e) => {
+    switch (e.target.value) {
+      case 'name-asc':
+        setSortBy({ sort: 'name', sortDir: 'asc' });
+        break;
+      case 'name-desc':
+        setSortBy({ sort: 'name', sortDir: 'desc' });
+        break;
+      default:
+        setSortBy();
+    }
+  };
 
   return (
     <PartnerWrapper title="Events">
@@ -48,6 +68,15 @@ function PartnerHome() {
           <Alert variant="danger">An error has occured</Alert>
         ) : (
           <>
+            <Row className="mb-4">
+              <Col xs={4} sm={3}>
+                <select className="custom-select" onChange={handleChange}>
+                  <option value="">Sort by</option>
+                  <option value="name-asc">Name - A to Z</option>
+                  <option value="name-desc">Name - Z to A</option>
+                </select>
+              </Col>
+            </Row>
             <Row>
               {data.pages.map((page, i) => (
                 <Fragment key={i}>
