5c6b7fa3b922b754c7719014c95e4f169d53a39f
marcusoyh <ongmarcus204@gmail.com>
2021-03-02 18:04:13 +0800

    upload image component added

--- components/createEvent/tabPanes/ImagesPane.js
+++ components/createEvent/tabPanes/ImagesPane.js
@@ -3,12 +3,98 @@ import Image from 'react-bootstrap/Image';
 import { Row, Col } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import PropTypes from 'prop-types';
+import { RMIUploader } from "react-multiple-image-uploader";
+import { uploadEventImage, uploadMultipleEventImage } from '../../../lib/query/eventApi';
 
 const ImagesPane = ({ register }) => {
 
     const [profilepicUrl, setProfilepicUrl] = useState(
         '../../../public/assets/images/defaultprofilepic.png'
     );
+
+    const dataSources = [
+        {
+          id: 1,
+          dataURL: "https://picsum.photos/seed/1/600",
+        },
+        {
+          id: 2,
+          dataURL: "https://picsum.photos/seed/2/600",
+        },
+        {
+          id: 3,
+          dataURL: "https://picsum.photos/seed/3/600",
+        },
+        {
+          id: 4,
+          dataURL: "https://picsum.photos/seed/4/600",
+        },
+        {
+          id: 5,
+          dataURL: "https://picsum.photos/seed/5/600",
+        },
+        {
+          id: 6,
+          dataURL: "https://picsum.photos/seed/6/600",
+        },
+        // {
+        //   id: 7,
+        //   dataURL: "https://picsum.photos/seed/7/600",
+        // },
+        // {
+        //   id: 8,
+        //   dataURL: "https://picsum.photos/seed/8/600",
+        // },
+        // {
+        //   id: 9,
+        //   dataURL: "https://picsum.photos/seed/9/600",
+        // },
+        // {
+        //   id: 10,
+        //   dataURL: "https://picsum.photos/seed/10/600",
+        // },
+      ];
+
+    const [visible, setVisible] = useState(false);
+    const handleSetVisible = () => {
+        console.log('setting visieble');
+        setVisible(true);
+    };
+    const hideModal = () => {
+        setVisible(false);
+    };
+    const onUpload = async (data) => {
+        // console.log('checking length');
+        // console.log(data.length);
+        // console.log('uploading separate');
+        // console.log(data[0].file);
+        // console.log(data);
+        // console.log("Upload files", data);
+        
+        //single file
+        let inputData = new FormData();
+        inputData.append('file',data[0].file);
+        console.log('checking inpu data');
+        console.log(inputData);
+        const response = await uploadEventImage(inputData);
+
+        //multi file upload
+        // let fileArray=[];
+        // let i;
+        // for (i=0;i < data.length; i++) {
+        //     fileArray.push(data[i].file);
+        // }
+        // inputData.append('files',fileArray);
+        // const response = await uploadMultipleEventImage(inputData);
+    };
+    const onSelect = (data) => {
+        // console.log("Select files", data);
+        console.log('insert into post method');
+        console.log(data);
+    };
+    const onRemove = (id) => {
+        console.log("Remove image id", id);
+    };
 
     return (
         <Card className="my-account-content__content">
@@ -36,9 +122,20 @@ const ImagesPane = ({ register }) => {
                             <Image
                                 className="profile-image"
                                 src={profilepicUrl}
-                                // thumbnail
+                            // thumbnail
                             />
                         </Col>
+                    </Row>
+                    <Row>
+                        <button onClick={() => handleSetVisible()}>Show Me</button>
+                        <RMIUploader
+                            isOpen={visible}
+                            hideModal={hideModal}
+                            onSelect={onSelect}
+                            onUpload={onUpload}
+                            onRemove={onRemove}
+                            dataSources={dataSources}
+                        />
                     </Row>
 
                 </div>
