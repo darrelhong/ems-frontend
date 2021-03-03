40104f3af01fc054b9bbcf6aaed3cbd31ce7ef7d
darrelhong <darrel.hong@gmail.com>
2021-03-03 23:45:48 +0800

    update email verificaiton error page

--- pages/register/error.js
+++ pages/register/error.js
@@ -1,14 +1,11 @@
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import Link from 'next/link';
-import { useState} from 'react';
-import HomeHeaderTop from '../../components/Header/HomeHeaderTop';
-// import { LayoutOne } from '../../layouts';
-import { Container, Row, Alert } from 'react-bootstrap';
 import GuestWrapper from '../../components/wrapper/GuestWrapper';
 import ButtonWithLoading from '../../components/custom/ButtonWithLoading';
 import { useMutation } from 'react-query';
 import api from '../../lib/ApiClient';
+import { Alert, Container } from 'react-bootstrap';
 
 export const getServerSideProps = async ({ query }) => {
   return {
@@ -23,87 +20,12 @@ export default function RegisterVerificationError({ token }) {
     api.get(`/api/user/register/resend?token=${token}`)
   );
 
-  const [showMsg, setShowMsg] = useState(true);
-
   return (
     <>
       <Head>
         <title>Email verification unsuccesful!</title>
       </Head>
-     
-{/* 
-      <GuestWrapper>
-        <Container>
-          <Row className="justify-content-center">
-            <p className="mt-5">
 
-              Your email could not be verified.{' '}
-              {token && 'Your verfication link may have expired.'}
-            &nbsp;
-          </p>
-          </Row>
-          &nbsp;
-          <br></br>
-          <Row className="justify-content-center">
-            {token ? (
-              <button className="btn btn-sm btn-fill-out"
-
-                isLoading={isLoading}
-                onClick={() => {
-                  mutate({ token });
-                }}
-              >
-                Resend verification email
-              </button>
-            ) : (
-                <Link href="/" passHref>
-                  <button className="btn btn-sm btn-fill-out" >Back to home</button>
-                </Link>
-              )}
-
-
-
-          </Row>
-          <Row className="justify-content-center">
-            <div>
-              &nbsp;
-              
-    {isError && (
-                // <Alert status="error">
-                //   <AlertIcon />
-                //   An error occured.
-                // </Alert>
-                <Alert
-                show={showMsg && isError}
-                  variant="danger"
-                  onClose={() => setShowMsg(false)}
-                  dismissible
-                >
-                  An error occured. Please try again.
-                </Alert>
-              )}
-              {isSuccess && (
-                // <Alert status="success">
-                //   <AlertIcon />
-                //   Verification email resent. Please check your inbox.
-                // </Alert>
-                <Alert
-                show={showMsg && isSuccess}
-                  variant="success"
-                  dismissible
-                  onClose={() => setShowMsg(false)}
-                >
-                  Verification email resent. Please check your inbox.
-                </Alert>
-              )}
-            </div>
-          </Row>
-
-
-
-        </Container>
-
-      </GuestWrapper> */}
       <GuestWrapper>
         <Container className="my-4">
           <div className="d-flex justify-content-center">
@@ -140,7 +62,7 @@ export default function RegisterVerificationError({ token }) {
             </div>
           </div>
         </Container>
-    </GuestWrapper>
+      </GuestWrapper>
     </>
   );
 }
