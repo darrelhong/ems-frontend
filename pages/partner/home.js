1dd3f4cab1f8ad1c8e9e9d57750bc322dd40f385
darrelhong <darrel.hong@gmail.com>
2021-03-03 23:45:35 +0800

    update partner home page

--- pages/partner/home.js
+++ pages/partner/home.js
@@ -1,36 +1,3 @@
-// import { Heading, Skeleton, Text } from '@chakra-ui/react';
-
-// import useUser from '../../lib/query/useUser';
-
-// import { PartnerPageWrapper } from '../../components/wrapper/PageWrapper';
-// import withProtectRoute from '../../components/ProtectRouteWrapper';
-// import ChakraWrapper from '../../components/ChakraWrapper';
-
-// function PartnerHome() {
-//   const { data: user, isLoading, isSuccess } = useUser(
-//     localStorage.getItem('userId')
-//   );
-//   return (
-//     <ChakraWrapper>
-//       <PartnerPageWrapper title="Partner Dashboard">
-//         <Heading>Partner Home</Heading>
-//         {isLoading && <Skeleton height="40px" />}
-//         {isSuccess && (
-//           <>
-//             <Text>Name: {user?.name}</Text>
-//             <Text>User ID: {user?.id}</Text>
-//           </>
-//         )}
-//       </PartnerPageWrapper>
-//     </ChakraWrapper>
-//   );
-// }
-
-// export default withProtectRoute(PartnerHome, {
-//   redirectTo: '/partner/login',
-// });
-
-
 import Link from 'next/link';
 import { Container } from 'react-bootstrap';
 
@@ -45,7 +12,7 @@ export default function PartnerHome() {
   );
   return (
     <PartnerWrapper title="Partner Home">
-      <BreadcrumbOne pageTitle={"Welcome " + user?.name} >
+      <BreadcrumbOne pageTitle="Partner Home">
         <ol className="breadcrumb justify-content-md-end">
           <li className="breadcrumb-item">
             <Link href="/partner/home">
@@ -61,16 +28,12 @@ export default function PartnerHome() {
           <>
             <p>{user?.name}</p>
             <p>User ID: {user?.id}</p>
-            <p>{user?.name}</p>
-            <p>User ID: {user?.id}</p>
-            <p>{user?.name}</p>
-            <p>User ID: {user?.id}</p>
-            <p>{user?.name}</p>
-            <p>User ID: {user?.id}</p>
-            <p>{user?.name}</p>
-            <p>User ID: {user?.id}</p>
           </>
         )}
+
+        <Link href="/partner/events">
+          <button className="btn btn-fill-out btn-sm">View events</button>
+        </Link>
       </Container>
     </PartnerWrapper>
   );
