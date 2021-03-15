6447142750e721969508f6e8b0f0af26b97e8a6a
marcusoyh <ongmarcus204@gmail.com>
2021-03-16 01:39:24 +0800

    using product images now

--- components/Booth/boothProfileDetails/BoothProductComponent.js
+++ components/Booth/boothProfileDetails/BoothProductComponent.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react';
+import ProductScrollView from './ProductScrollView';
 
-const BoothProductComponent = ({booth}) => {
-    const [products,setProducts] = useState([]);
+const BoothProductComponent = ({ booth }) => {
+    const [products, setProducts] = useState([]);
     const [boothData, setBooth] = useState(Object);
     // const [slicedProducts,setSlicedProducts] = useState([[]]);
     // const [productsWithKeys,setProductsWithKeys] = useState([]);
     const [paginatedProducts, setPaginatedProducts] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
+        console.log('checking booth products');
+        console.log(booth?.products?.forEach((prod)=>console.log(prod?.name)));
         const loadData = () => {
             setBooth(booth);
             const boothProducts = booth.products;
@@ -19,21 +22,35 @@ const BoothProductComponent = ({booth}) => {
             }
             let counter = 0;
             for (const productSet of slicedProducts) {
+                console.log('appending');
+                console.log(productSet);
                 const input = {
-                    "key" : booth.boothNumber + ' ' + counter,
-                    "products" : productSet
+                    "key": booth.boothNumber + '' + counter,
+                    "products": productSet
                 };
                 //idk why first array is always empty 
-                if (counter!=0) productsWithKeys.push(input); 
-                counter+=1;
+                if (counter != 0) productsWithKeys.push(input);
+                counter += 1;
             }
             setPaginatedProducts(productsWithKeys);
+            console.log('checking prods');
+            console.log(productsWithKeys);
+            // for (const products in productsWithKeys) {
+            //     console.log(products);
+            // }
         };
         if (booth) loadData();
-    },[booth]);
+    }, [booth]);
 
     return (
-        <h4>Booth {booth?.boothNumber}</h4>
+        <div>
+            <h4>Booth {booth?.boothNumber}</h4>
+            {
+                paginatedProducts && (
+                    <ProductScrollView paginatedProducts={paginatedProducts} />
+                )
+            }
+        </div>
     )
 };
 
