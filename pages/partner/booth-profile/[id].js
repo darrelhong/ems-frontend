e05b884a714758641214c8395bd3ede342f7c5d2
marcusoyh <ongmarcus204@gmail.com>
2021-03-15 19:36:23 +0800

    added booth fetching w products

--- pages/partner/booth-profile/[id].js
+++ pages/partner/booth-profile/[id].js
@@ -1,24 +1,36 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { getBoothProfile } from 'lib/query/boothApi';
+import { getBoothProfile, getBoothsByBoothProfile } from 'lib/query/boothApi';
 
 const BoothProfile = () => {
     const router = useRouter();
     const { id } = router.query;
-    const [boothProfile,setBoothProfile] = useState(Object);
-    
-    useEffect(()=>{
+    const [boothProfile, setBoothProfile] = useState(Object);
+    const [booths, setBooths] = useState([]);
+
+    useEffect(() => {
         const loadData = async () => {
-            const data = await getBoothProfile(1);
-            setBoothProfile(data);
+            const boothProfileData = await getBoothProfile(id);
+            setBoothProfile(boothProfileData);
+            const boothData = await getBoothsByBoothProfile(id);
+            setBooths(boothData);
         }
         loadData();
-    },[]);
+    }, []);
 
     return (
         <div>
             <h1>aye lmao {id}</h1>
-            <h6>{boothProfile?.description ?? 'lmaooo'}</h6>
+            <h6>Booth Profile description: {boothProfile?.description ?? 'lmaooo'}</h6>
+            {booths && booths.map((booth) => (
+                <div>
+                    <h6>Booth number: {booth?.boothNumber ?? 'lmaooo'}</h6>
+                    {booth.products && booth.products.map((product) => (
+                        <h6>Product name: {product?.name ?? 'lmaooo'}</h6>
+                    ))
+                    }
+                </div>
+            ))}
         </div>
     )
 }
