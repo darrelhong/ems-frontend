ac562dc48f7e930bf8be5a3cc92dd4ca9906b2ea
darrelhong <darrel.hong@gmail.com>
2021-03-20 12:59:13 +0800

    add select for previous and upcoming events

--- pages/attendee/tickets.js
+++ pages/attendee/tickets.js
@@ -8,14 +8,18 @@ import AttendeeWrapper from 'components/wrapper/AttendeeWrapper';
 import { Alert, Col, Container, Row } from 'react-bootstrap';
 import CenterSpinner from 'components/custom/CenterSpinner';
 import TicketCard from 'components/custom/ticketing/TicketCard';
+import { useState } from 'react';
 
-const getTickets = async () => {
-  const { data } = await api.get('/api/ticketing/attendee');
+const getTickets = async (period) => {
+  const { data } = await api.get(`/api/ticketing/attendee?period=${period}`);
   return data;
 };
 
 export default function AttendeeTickets() {
-  const { data, status } = useQuery('tickets', getTickets);
+  const [period, setPeriod] = useState('upcoming');
+  const { data, status } = useQuery(['tickets', period], () =>
+    getTickets(period)
+  );
 
   return (
     <AttendeeWrapper title="View my tickets">
@@ -31,13 +35,22 @@ export default function AttendeeTickets() {
       </BreadcrumbOne>
 
       <Container className="my-4">
+        <Row className="mb-3" onChange={(e) => setPeriod(e.target.value)}>
+          <Col sm={4}>
+            <select className="custom-select">
+              <option value="upcoming">Upcoming</option>
+              <option value="previous">Previous</option>
+            </select>
+          </Col>
+        </Row>
+
         {status == 'loading' ? (
           <CenterSpinner />
         ) : status == 'error' ? (
           <Alert variant="danger">An error has occured</Alert>
         ) : (
           <>
-            <Row className="mt-3">
+            <Row>
               {data.map((ticket) => (
                 <Col md={6} key={ticket.id} className="mb-4">
                   <TicketCard ticket={ticket} />
