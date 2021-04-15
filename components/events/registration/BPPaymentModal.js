8213a156a65e4bb341ad3a1bbb5932c8398d3ec5
keithhjc <keithhjc@gmail.com>
2021-04-15 11:50:06 +0800

    test

--- components/events/registration/BPPaymentModal.js
+++ components/events/registration/BPPaymentModal.js
@@ -17,7 +17,8 @@ const BPPaymentModal = ({
     closePaymentModal
 }) => {
 
-    console.log(event)
+    const [pmId, setPmId] = useState();
+    const { data: paymentMethods, status: pmStatus } = usePaymentMethods();
     const [view, setView] = useState('summary');
 
     const bodyComponent = () => (
@@ -51,6 +52,77 @@ const BPPaymentModal = ({
                                             <strong>Allocated Booths: </strong>
                                             { }
                                         </p>
+                                    </Col>
+                                </Row>
+
+                                <Row className="mb-5">
+                                    <Col>
+                                        <h4>Order Summary:</h4>
+                                    </Col>
+                                </Row>
+
+                                <Row>
+                                    <Col>
+                                        <p className="text-dark">
+                                            x Number of Booths
+                                    </p>
+                                    </Col>
+                                    <Col className="col-auto">
+                                        <p className="text-dark">
+                                            {formatter.format(event?.boothPrice)}
+                                        </p>
+                                    </Col>
+                                </Row>
+                                <hr></hr>
+                                <Row className="mb-4">
+                                    <Col>
+                                        <p className="h4 text-body">Total</p>
+                                    </Col>
+                                    <Col className="col-auto">
+                                        <p className="h5 text-body">
+                                            {formatter.format(event?.boothPrice)}
+                                        </p>
+                                    </Col>
+                                </Row>
+
+                                <h5 className="mb-3">Payment method:</h5>
+                                <Row>
+                                    <Col>
+                                        {paymentMethods?.map((pm) => (
+                                            <div className="form-check mb-3" key={pm.id}>
+                                                <input
+                                                    className="form-check-input"
+                                                    type="radio"
+                                                    name="paymentCard"
+                                                    id="newCard"
+                                                    checked={pmId === pm.id}
+                                                    onChange={() => handleCardChange(pm.id)}
+                                                />
+                                                <label
+                                                    className="form-check-label"
+                                                    htmlFor="exampleRadios1"
+                                                >
+                                                    <CreditCardIcon type={pm.card.brand} />
+                              Card ending in {pm.card.last4}
+                                                </label>
+                                            </div>
+                                        ))}
+                                        <div className="form-check mb-4">
+                                            <input
+                                                className="form-check-input"
+                                                type="radio"
+                                                onChange={() => handleCardChange(null)}
+                                                name="paymentCard"
+                                                id="newCard"
+                                                checked={!pmId}
+                                            />
+                                            <label
+                                                className="form-check-label"
+                                                htmlFor="exampleRadios1"
+                                            >
+                                                Use new card
+                          </label>
+                                        </div>
                                     </Col>
                                 </Row>
                             </>
